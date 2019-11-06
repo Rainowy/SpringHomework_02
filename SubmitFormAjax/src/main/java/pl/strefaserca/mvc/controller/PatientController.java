ed82d4e1c78398dd8e58121aa9e4d1c2f6e45fba
Rainowy <tomasz.czarny.czarnecki@gmail.com>
2019-11-06 20:54:57 +0100

    Ajax Post not ready yet.

--- SubmitFormAjax/src/main/java/pl/strefaserca/mvc/controller/PatientController.java
+++ SubmitFormAjax/src/main/java/pl/strefaserca/mvc/controller/PatientController.java
@@ -1,18 +1,48 @@
 package pl.strefaserca.mvc.controller;
 
+import com.sun.xml.internal.bind.v2.TODO;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.annotation.*;
+import pl.strefaserca.mvc.model.Patient;
+import pl.strefaserca.mvc.model.PatientJsonResponse;
+
+import javax.validation.Valid;
+import java.awt.*;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @Controller
-@RequestMapping("/account")
+//@RequestMapping("/account")
 public class PatientController {
 
-    @GetMapping("/ku")
-    @ResponseBody
+    @GetMapping("/")
     public String patientForm() {
-//        return "patientForm";
-        return "SIEMANKO";
+        return "patientForm";
+    }
+
+    @PostMapping(value = "/savePatient", produces = {MediaType.APPLICATION_JSON_VALUE})
+    @ResponseBody
+    public PatientJsonResponse savePatient(@ModelAttribute @Valid Patient patient,
+                                           BindingResult result) {
+
+        PatientJsonResponse response = new PatientJsonResponse();
+
+        if (result.hasErrors()) {
+
+            Map<String, String> errors = result.getFieldErrors().stream()
+                    .collect(Collectors.toMap(FieldError::getField, FieldError::getDefaultMessage));
+
+            response.setValidated(false);
+            response.setErrorMessages(errors);
+        } else {
+            //TODO zapisanie pacjenta do bazy
+
+            response.setValidated(true);
+            response.setPatient(patient);
+        }
+        return response;
     }
 }
--- SubmitFormAjax/src/main/webapp/WEB-INF/views/patientForm.jsp
+++ SubmitFormAjax/src/main/webapp/WEB-INF/views/patientForm.jsp
@@ -9,6 +9,48 @@
 <html>
 <head>
     <title>strefaserca.pl</title>
+
+    <style type="text/css">
+        span.error {
+            color: red;
+            margin-left: 5px;
+        }
+    </style>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script type="text/javascript">
+
+        $(function () {
+            /*Submit form using Ajax */
+
+            $('button[type=submit]').click(function (e) {
+
+                //Prevent default submission of form
+                e.preventDefault();
+
+                //Remove all errors
+                $('input').next().remove();
+
+                $.post({
+                    url: 'savePatient',
+                    data: $('form[name=patientForm]').serialize(),
+                    success: function (res) {
+
+                        if (res.validated) {
+                            $('#resultContainer pre code').text(JSON.stringify(res.patient));
+                            $('#resultContainer').show();
+                        }
+
+
+                    }
+                }
+
+            })
+
+        })
+
+
+    </script>
+
 </head>
 <body>
 <h1>hello</h1>
